4ade489417c40f0592f03bb2fba5742ab4c7637a
TheoOliveira <theophilo.oliveira@gmail.com>
2022-09-09 23:39:36 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const db = require('./app/models')
+const corsOptions = {
+    origin: 'http://localhost:8081'
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//sequelize init
+db.sequelize.sync({force: true})
+    .then(()=> {
+        console.log('Synced DB')
+    })
+    .catch((e)=> {
+        console.error('Failed sync db :', +e.message)
+    })
+
+app.get('/', (req, res)=> {
+    res.json({message: 'Welcome to theo app todo'})
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
